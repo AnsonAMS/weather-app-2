dfd54e3c3e29ee053b309462ec78e85fdc079419
AnsonAMS <anson_tohata@hotmail.com>
2021-01-12 18:53:54 +0100

    Updating app.js with ternary operator

--- scripts/app.js
+++ scripts/app.js
@@ -4,46 +4,52 @@ const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
 
-
 const updateUI = (data) => {
-    //destructure properties
-    const { cityDets, weather } = data;
+  // destructure properties
+  const { cityDets, weather } = data;
 
-    // update details template
-    details.innerHTML = `
-    <h5 class="my3">${cityDets.EnglishName}</h5>
+  // update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
     <div class="my-3">${weather.WeatherText}</div>
     <div class="display-4 my-4">
-        <span>${weather.Temperature.Metric.Value}</span>
-        <span>&deg;C</span>
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
     </div>
-    `;
+  `;
 
-    // remove d-none class if present
-    if(card.classList.contains('d-none')) {
-        card.classList.remove('d-none');    
-    }
+  // update the night/day & icon images
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+  
+  let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+  
+  time.setAttribute('src', timeSrc);
 
+  // remove the d-none class if present
+  if(card.classList.contains('d-none')){
+    card.classList.remove('d-none');
+  }
 };
 
 const updateCity = async (city) => {
 
-    const cityDets = await getCity(city);
-    const weather = await getWeather(cityDets.Key);
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+  return { cityDets, weather };
 
-    return { cityDets, weather };
 };
 
 cityForm.addEventListener('submit', e => {
-    //prevent default action
-    e.preventDefault();
+  // prevent default action
+  e.preventDefault();
+  
+  // get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
 
-    // get city value
-    const city = cityForm.city.value.trim();
-    cityForm.reset();
-
-    //update the ui with new city 
-    updateCity(city)
-        .then(data => updateUI(data))
-        .catch(err => console.log(err));
+  // update the ui with new city
+  updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
 });
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,17 +1,5 @@
 const key = '1pMXCqPVfr8bUtu6AIpOJPP2SMB42MBR';
 
-// // get weather information
-// const getWeather = async (id) => {
-
-//     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';  
-//     const query = `${id}?apikey=${key}`;
-
-//     const response = await fetch(base + query);
-//     const data = await response.json();
-
-//     return data[0];
-// };
-
 // get weather information
 const getWeather = async (id) => {
   
